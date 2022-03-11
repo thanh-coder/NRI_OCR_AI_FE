ce9cc61712545823b1dfc60ca079568acca0541d
thanh-coder <tranhung050990@gmail.com>
2022-03-11 11:31:50 +0700

    update dimension of image

--- utils/handleScanDriver.js
+++ utils/handleScanDriver.js
@@ -1,6 +1,6 @@
 let video;
 let webcamStream;
-let canvas, ctx;
+let canvas, ctx, canvas1, ctx1;
 let modalDetailInfor;
 let btnStartCamElm;
 let btnStopCamElm;
@@ -30,26 +30,28 @@ const startWebcam = () => {
           height: { ideal: 720 },
         },
   };
-  
-  navigator.getUserMedia = ( navigator.getUserMedia ||
+
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
+    navigator.msGetUserMedia;
 
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((localMediaStream) => {
+  navigator.getUserMedia(
+    constraints,
+    (localMediaStream) => {
       video.srcObject = localMediaStream;
       webcamStream = localMediaStream;
       btnTakeSnapshotElm.classList.remove("disabled");
       btnStopCamElm.classList.remove("disabled");
       btnStopCamElm.disabled = false;
       btnTakeSnapshotElm.disabled = false;
-    })
-    .catch((error) => {
-      console.log("The following error occured: " + err);
-    });
-}
+    },
+    (error) => {
+      console.log("The following error occured: " + error);
+    }
+  );
+};
 
 const stopWebcam = () => {
   webcamStream.getTracks().forEach((track) => {
@@ -61,11 +63,13 @@ const stopWebcam = () => {
   btnStopCamElm.classList.add("disabled");
   btnStopCamElm.disabled = true;
   btnTakeSnapshotElm.disabled = true;
-}
+};
 
 const initWebPage = () => {
   canvas = document.createElement("canvas");
   ctx = canvas.getContext("2d");
+  canvas1 = document.getElementById("myCanvas");
+  ctx1 = canvas1.getContext("2d");
   canvas.width = 500;
   canvas.height = 600;
   btnTakeSnapshotElm = document.querySelector(".btn-take-snapshot");
@@ -79,7 +83,7 @@ const initWebPage = () => {
   video.setAttribute("autoplay", "");
   video.setAttribute("muted", "");
   video.setAttribute("playsinline", "");
-}
+};
 
 const snapshotPicture = () => {
   if (webcamStream) {
@@ -87,8 +91,15 @@ const snapshotPicture = () => {
     btnTakeSnapshotElm.classList.add("disabled");
     btnStopCamElm.disabled = true;
     btnTakeSnapshotElm.disabled = true;
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.width);
+    var img_w = video.videoWidth;
+    var img_h = video.videoHeight;
+    canvas.width = img_w;
+    canvas.height = img_h;
+    // canvas1.width = img_w;
+    // canvas1.height = img_h;
+    // ctx1.drawImage(video, 0, 0, img_w, img_h);
+    ctx.drawImage(video, 0, 0, img_w, img_h);
+    const imageData = ctx.getImageData(0, 0, img_w, img_h);
     const uint8ArrData = new Uint8Array(imageData.data);
     scanVideoElm = document.querySelector(".cam-effect-scan");
     loadingSpinnerElm = document.querySelector(".cam-loading-spinner");
@@ -105,63 +116,67 @@ const snapshotPicture = () => {
       scanVideoElm.classList.remove("active");
       clearInterval(scanProgressInterval);
       percenScan = 0;
-    }, 1100)
+    }, 1100);
     setTimeout(() => {
-      passToWasm(uint8ArrData, canvas.width, canvas.height);
+      passToWasm(uint8ArrData, img_w, img_h);
     }, 1200);
-    
   }
-}
+};
 
 const handleCloseModal = () => {
   modalDetailInfor.hide();
 };
 
+Module.onRuntimeInitialized = () => {};
 const passToWasm = (imageData, wid, hig) => {
-  const { length } = imageData;
-  const memory = Module._malloc(length); // Allocating WASM memory
-  HEAPU8.set(imageData, memory); // Copying JS image data to WASM memory
-  let errorMessageAray = new Uint8Array(1000);
-  let errorMessagePointer = _malloc(1000 * Uint8Array.BYTES_PER_ELEMENT);
-  HEAPU8.set(
-    errorMessageAray,
-    errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT
-  );
+  try {
+    const { length } = imageData;
+    const memory = Module._malloc(length); // Allocating WASM memory
+    HEAPU8.set(imageData, memory); // Copying JS image data to WASM memory
+    let errorMessageAray = new Uint8Array(1000);
+    let errorMessagePointer = _malloc(1000 * Uint8Array.BYTES_PER_ELEMENT);
+    HEAPU8.set(
+      errorMessageAray,
+      errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT
+    );
 
-  let payloadArray = new Uint8Array(1000);
-  let payloadPointer = _malloc(1000 * Uint8Array.BYTES_PER_ELEMENT);
-  HEAPU8.set(payloadArray, payloadPointer / Uint8Array.BYTES_PER_ELEMENT);
+    let payloadArray = new Uint8Array(1000);
+    let payloadPointer = _malloc(1000 * Uint8Array.BYTES_PER_ELEMENT);
+    HEAPU8.set(payloadArray, payloadPointer / Uint8Array.BYTES_PER_ELEMENT);
 
-  _ocr_ncnn(memory, wid, hig, payloadPointer, errorMessagePointer);
+    _ocr_ncnn(memory, wid, hig, payloadPointer, errorMessagePointer);
 
-  let errorMessageResult = HEAPU8.subarray(
-    errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT,
-    errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT + 1000
-  );
-  errorMessageString = new TextDecoder("utf8")
-    .decode(errorMessageResult)
-    .trim();
-  errorMessageString = errorMessageString
-    .replaceAll("\u0000", "")
-    .replaceAll("\r", "");
+    let errorMessageResult = HEAPU8.subarray(
+      errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT,
+      errorMessagePointer / Uint8Array.BYTES_PER_ELEMENT + 1000
+    );
+    errorMessageString = new TextDecoder("utf8")
+      .decode(errorMessageResult)
+      .trim();
+    errorMessageString = errorMessageString
+      .replaceAll("\u0000", "")
+      .replaceAll("\r", "");
 
-  let payloadResult = HEAPU8.subarray(
-    payloadPointer / Uint8Array.BYTES_PER_ELEMENT,
-    payloadPointer / Uint8Array.BYTES_PER_ELEMENT + 1000
-  );
-  payloadString = new TextDecoder("utf8").decode(payloadResult).trim();
-  payloadString = payloadString.replaceAll("\u0000", "").replaceAll("\r", "");
-  payloadObject = JSON.parse(payloadString);
-  resetEffectScanImg();
-  if (payloadString && !errorMessageString) {
-    onSucess(payloadString);
-  } else {
-    onError();
+    let payloadResult = HEAPU8.subarray(
+      payloadPointer / Uint8Array.BYTES_PER_ELEMENT,
+      payloadPointer / Uint8Array.BYTES_PER_ELEMENT + 1000
+    );
+    payloadString = new TextDecoder("utf8").decode(payloadResult).trim();
+    payloadString = payloadString.replaceAll("\u0000", "").replaceAll("\r", "");
+    payloadObject = JSON.parse(payloadString);
+    resetEffectScanImg();
+    if (payloadString && !errorMessageString) {
+      onSucess(payloadString);
+    } else {
+      onError();
+    }
+    Module._free(memory); // Freeing WASM memory
+    Module._free(errorMessagePointer); // Freeing WASM memory
+    Module._free(payloadPointer); // Freeing WASM memory
+  } catch (error) {
+    console.log("error========", error);
   }
-  Module._free(memory); // Freeing WASM memory
-  Module._free(errorMessagePointer); // Freeing WASM memory
-  Module._free(payloadPointer); // Freeing WASM memory
-}
+};
 
 const resetEffectScanImg = () => {
   loadingSpinnerElm.classList.add("d-none");
@@ -170,7 +185,7 @@ const resetEffectScanImg = () => {
   btnStopCamElm.disabled = false;
   btnTakeSnapshotElm.classList.remove("disabled");
   btnTakeSnapshotElm.disabled = false;
-}
+};
 
 const onSucess = () => {
   Swal.fire({
